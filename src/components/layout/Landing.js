ffacab944b4c0d73b432d5bf626a71babc99795c
Abraham E. Tavarez <abetavarez@icloud.com>
2022-06-25 21:38:26 -0400

    navbar and landing components created, react router dom init

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,16 @@
+const Landing = () => {
+  return (
+    <section>
+      <h1>OP BLACKLIST APP</h1>
+
+      <p>Create an account to get started</p>
+
+      <div>
+        <a href="index.html">Register</a>
+        <a href="index.html">Login</a>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>OP BLACKLIST APP</h1>
+      <ul>
+        <il>
+          <a href="index.html">Home</a>
+        </il>
+        <il>
+          <a href="index.html">Register</a>
+        </il>
+        <il>
+          <a href="index.html">Login</a>
+        </il>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
