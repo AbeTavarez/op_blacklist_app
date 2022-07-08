21e33fad6e9203441ddef89d72f768a6923fe225
Abraham E. Tavarez <abetavarez@icloud.com>
2022-07-07 22:08:00 -0400

    redux toolkit installed and init.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -7,10 +8,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: middleware,
+  devTools: true,
+  preloadedState: initialState,
+});
 
 export default store;
